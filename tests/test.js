1b8bb5d056df805443414123fbc6662acb55d4a6
Stephanie Miranda <stephanie.miranda.valdez@gmail.com>
2016-01-26 12:13:49 -0600

    test in progress

--- tests/test.js
+++ tests/test.js
@@ -1,9 +1,13 @@
 'use strict';
 
 const {expect} = require('chai');
+const {execSync} = require('child_process');
 
 describe('test suite', () => {
-  it('true should be true', () => {
-    expect(true).to.be.true;
+  it('should handle the current month', () => {
+    const goal = execSync('cal').toString();
+    const output = execSync('./cal.js').toString();
+
+    expect(output).to.equal(goal);
   });
 });
